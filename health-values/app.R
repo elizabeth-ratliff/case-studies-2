0d81c5bada8dd2e2c1634b0b400d886c3060c385
Eidan Jacob <ej68@duke.edu>
2018-09-26 19:06:46 -0400

    added basic UI elements

--- health-values/app.R
+++ health-values/app.R
@@ -3,6 +3,8 @@ library(tidyverse)
 health <- read_csv("../data/ghdx.csv")
 values <- read_csv("../data/wvsData.csv")
 
+valueCountries <- sort(unique(values$Country))
+
 ui <- fluidPage(
    
    titlePanel("Global Women's Health and World Values"),
@@ -10,15 +12,32 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
         
+        selectInput("countries_vTab", 
+                    "Countries", 
+                    choices = c("All", valueCountries),
+                    selected = "All",
+                    multiple = TRUE),
+        
+        checkboxGroupInput("value1_vTab",
+                    "Justifiable?",
+                    choices = names(values)[c(-1, -5)],
+                    selected = NULL)
       ),
       
       mainPanel(
+        
+        plotOutput("valuesPlot")
+        
       )
    )
 )
 
 server <- function(input, output) {
    
+  output$valuesPlot <- renderPlot({
+    ggplot()
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
